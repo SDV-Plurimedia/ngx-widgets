0bc7945c83c3d6c27a7c15134920234a5ca6aa12
Antoine WEBER <a.weber@sdv.fr>
2017-05-16 13:51:56 +0200

    maj du script de deploiement de la doc

--- bin/deploy-pages.sh
+++ bin/deploy-pages.sh
@@ -2,7 +2,10 @@
 
 # if anything in the site/ subdirectory changed in the prior commit,
 # push that directory to gh-pages for auto generation.
-git diff-tree -r --name-only --no-commit-id master | grep '^documentation/dist/' &> /dev/null
+git diff-tree -r --name-only --no-commit-id develop | grep '^documentation/dist/' &> /dev/null
 if [ $? == 0 ]; then
-  git push origin `git subtree split --prefix documentation/dist master 2> /dev/null`:gh-pages --force
+  git push origin `git subtree split --prefix documentation/dist develop 2> /dev/null`:gh-pages --force
+  echo "La documentation a ete mis à jour"
+else
+  echo "Pas de changement dans la doc"
 fi
