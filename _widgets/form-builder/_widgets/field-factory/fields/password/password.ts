01526c7057fa05387b79c5229e6f079377b9134d
Jean-Philippe Schreiber <jpdu67@hotmail.fr>
2017-10-29 19:37:25 +0100

    Autocomplete : Plus de sort sur la colonne des boutons. FormBuilder : Input password

---
+++ _widgets/form-builder/_widgets/field-factory/fields/password/password.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {FieldComponent} from '../field';
+
+@Component({
+    selector: 'field-password',
+    templateUrl: './password.html'
+})
+
+export class FieldPasswordComponent extends FieldComponent {
+
+}
