45c5f71cf446a6ce5eb23a3cefb92d2128487e00
mburger <mburger@sdv.fr>
2018-02-27 16:34:40 +0100

    Added missing doc

--- _widgets/tabpane/tabpane.ts
+++ _widgets/tabpane/tabpane.ts
@@ -78,12 +78,13 @@ export class TabpaneComponent implements AfterContentInit {
   ngAfterContentInit() {
     // Premier des composants descendants lors du premier chargement
     this._calcDirectChild();
-
+    // on recalcul a chaque changement des enfants, si on s'est pas encore inscrit sur cette querylist
     if (this.directTabs) {
       if (
         typeof this.directTabs.changes['observers'] === 'undefined' ||
         this.directTabs.changes['observers'].length === 0
       ) {
+        // si un autre querylist étais déjà entrain d'etre écouté
         if (this._subDirectTabs) {
           this._subDirectTabs.unsubscribe();
         }
@@ -125,14 +126,17 @@ export class TabpaneComponent implements AfterContentInit {
      * (résoud le problème de doCheck)
      */
     setTimeout(() => {
+      // On marque tous les enfants direct avec ce flag
       this.directTabs.forEach(
         (tab: TabComponent) => (tab.parentRandId = this._rand)
       );
       if (this.tabs && this.tabs.length) {
+        // Puis on selectionne tous les efants qui ont ce flag, ou qui n'en ont pas
         this._directChild = this.tabs.filter((tab: TabComponent) => {
           return tab.parentRandId === null || tab.parentRandId === this._rand;
         });
-
+        // Comme le AfterContentInit bubble depuis les enfants, les enfants auront déjà flagué leur propres enfants directes
+        // puis on marque même les enfants indirect (pour ne pas les chopper aussi dans un parent)
         this._directChild.forEach(
           (tab: TabComponent) => (tab.parentRandId = this._rand)
         );
